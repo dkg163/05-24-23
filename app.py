ff84cf3aa73b9dbd62043ace27dbe744ea39a76e
dkg163 <134477193+dkg163@users.noreply.github.com>
2023-05-27 04:55:14 +0000

    5-26-2023 change
    5-26-2023 change

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'data analyst',
+    'location': 'SFO',
+    'salary': '100,000'
+  }
+    {
+    'id': 2,
+    'title': 'data analyst 2',
+    'location': 'SJC',
+    'salary': '150,000'
+  }
+  {
+    'id': 3,
+    'title': 'data analyst 3',
+    'location': 'OAK',
+    'salary': '120,000'
+  }
+]
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return render_template('t1.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug = True)
--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'data analyst',
+    'location': 'SFO',
+    'salary': '100,000'
+  },
+    {
+    'id': 2,
+    'title': 'data analyst 2',
+    'location': 'SJC',
+    'salary': '150,000'
+  },
+  {
+    'id': 3,
+    'title': 'data analyst 3',
+    'location': 'OAK',
+    'salary': '120,000'
+  },
+]
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return render_template('t1.html', jobs = JOBS, company_name = 'Jovian')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug = True)
-
-#  
-#  $ flask --app hello run
-# * Serving Flask app 'hello'
-# * Running on http://127.0.0.1:5000 (Press CTRL+C to quit)
